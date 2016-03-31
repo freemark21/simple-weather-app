b0eb07c0492389fe37579ef6fbe9a6560c0f2c6e
freemark21 <freemark21@gmail.com>
2016-03-30 20:05:41 -0500

    initial commit, api loaded

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-$.ready(function() {
+$(function() {
+    var apiKey = "7b21daac2891491963f09e298f79e97e";
     
-   console.log("hello"); 
+    function getWeather(){
+        $.getJSON('//api.openweathermap.org/data/2.5/weather?zip=55344,us&APPID=7b21daac2891491963f09e298f79e97e')
+        .done(function(data){
+            console.log(data);
+        }).fail(function(){
+            alert('Something went wrong');
+        });
+    };
     
-    
+getWeather();    
 });
+
+//api key 7b21daac2891491963f09e298f79e97e
