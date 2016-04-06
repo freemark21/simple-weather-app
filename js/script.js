55d50b657b49503d0b183c7bb2a2b3436b1261a6
Ryan Freemark <freemark21@gmail.com>
2016-04-06 17:32:38 -0500

    meh

--- js/script.js
+++ js/script.js
@@ -1,34 +1,25 @@
 $(function() {
     var apiKey = "7b21daac2891491963f09e298f79e97e";
     
-    function getWeather(){
-        $.getJSON('//api.openweathermap.org/data/2.5/weather?zip=' + '55344' + ',us&units=imperial&APPID=7b21daac2891491963f09e298f79e97e')
+    function getWeather(zipcode){
+        $.getJSON('//api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&APPID=7b21daac2891491963f09e298f79e97e')
         .done(function(data){
-            $('#weather').append('<p>The temp is ' + data.main.temp + '</p>');
+            $('#weather').append('<p>The temp is ' + data.main.temp + ' degrees farenheit</p>');
             $('#weather').append('<p>' + data.weather[0].description + '</p>');
         }).fail(function(){
             alert('Something went wrong');
         });
     };
+      
     
+$('#submit').on('submit', function(){
+    var zip = $('#zip').val();
+    getWeather(zip);
+    console.log(zip);
+});
+    //above code block not working calling getWeather function below for 55344
     
-getWeather();    
-    
-    
-    
-    
-    
-//var zip = ""    
-//$('#submit').on('submit', function(){
-//    $.preventDefault();
-//    zip = $('#zip').val();
-//    if (zip.length > 0){
-//        getWeather();
-//  } else {
-//        alert('Please enter a zip');
-//    };
-//});
-    
+    getWeather(55344)
        
 });
 
