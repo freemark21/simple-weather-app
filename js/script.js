5d02e987b71754b81cbb8f6c9e858911d6374fae
freemark21 <freemark21@gmail.com>
2016-04-13 19:01:02 -0500

    fixed

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 $(function() {
-    var apiKey = "7b21daac2891491963f09e298f79e97e";
     
-    function getWeather(zipcode){
-        $.getJSON('//api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&APPID=7b21daac2891491963f09e298f79e97e')
+    function getWeather(zipcode, apikey){
+        $.getJSON('//api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&APPID=' + apikey)
         .done(function(data){
             $('#weather').append('<p>The temp is ' + data.main.temp + ' degrees farenheit</p>');
             $('#weather').append('<p>' + data.weather[0].description + '</p>');
         }).fail(function(){
-            alert('Something went wrong');
+            alert('Something went wrong. Please check your zip code and api key.');
         });
     };
       
     
-$('#submit').on('submit', function(){
+$('#submit').on('click', function(){
     var zip = $('#zip').val();
-    getWeather(zip);
+    var api = $('#api').val();
+    getWeather(zip, api);
 });
-    //above code block not working calling getWeather function below for 55344
-    
-    getWeather(55344);
-       
+   
 });
-
-//api key 7b21daac2891491963f09e298f79e97e
